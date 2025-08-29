fe3556719a3cb2d048bdec458a97ef86035d64d7
RubenBlades2022 <142459408+RubenBlades2022@users.noreply.github.com>
2025-08-29 13:19:23 -0500

    userModel.js

--- src/modules/users/models/userModel.js
+++ src/modules/users/models/userModel.js
@@ -4,7 +4,21 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   role: { type: String, enum: ['player', 'admin', 'staff'], default: 'player' },
-  suspended: { type: Boolean, default: false }
+  suspended: { type: Boolean, default: false },
+
+  motivo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Motivo',
+    default: null
+  },
+  fechaSuspension: {
+    type: Date,
+    default: null
+  },
+  fechaReactivacion: {
+    type: Date,
+    default: null
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
