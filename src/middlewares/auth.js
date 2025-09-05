f64d8396a6c6e180dbd0e5fbcf32d4558c5ed83d
AndresPico <145989957+AndresPico@users.noreply.github.com>
2025-09-04 19:21:58 -0500

    Delete src directory

--- src/middlewares/auth.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  const role = req.header('x-role');
-  if (!role) return res.status(401).json({ message: 'No autenticado' });
-  req.user = { role };
-  next();
-};
